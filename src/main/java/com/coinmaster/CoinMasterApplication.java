ab736288079e61321b4cc60ccdbe885feda08fb4
johntboyle <83061760+johntboyle@users.noreply.github.com>
2022-01-25 06:38:33 -0600

    removed spring security

--- src/main/java/com/coinmaster/CoinMasterApplication.java
+++ src/main/java/com/coinmaster/CoinMasterApplication.java
@@ -2,23 +2,10 @@ package com.coinmaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CoinMasterApplication {
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*").allowedHeaders("*").allowedOriginPatterns("*").allowedMethods("*");
-			}
-		};
-	}
-	
 	public static void main(String[] args) {
 		SpringApplication.run(CoinMasterApplication.class, args);
 	}
